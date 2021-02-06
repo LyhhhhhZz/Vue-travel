e3d211f9beb108891ee3b3815a9f8825b85cfe68
lyh <1163697514@qq.com>
2021-02-06 23:22:18 +0800

    add raiders

---
+++ src/pages/home/components/Raiders.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="radiers">
+    <div class="title">旅游攻略</div>
+    <div class="radiers-title">趣味亲子游</div>
+    <div class="radiersItem" v-for="item of radiersList" :key="item.id">
+      <div class="radiersItem-img">
+        <img :src="item.imgUrl" class="radiersItem-img-content">
+      </div>
+      <div class="radiersItem-content">
+        <p class="radiersItem-desc">{{item.desc}}</p>
+      </div>
+    </div>
+    <button class="radiers-btn">更多精彩攻略 ></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRaiders',
+  data () {
+    return {
+      radiersList: [{
+        id: '0001',
+        imgUrl: '//tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/7d9b233c71cad13ded0013f9c9cec635.jpg',
+        desc: '驾一叶扁舟，寻瓯江往事'
+      },
+      {
+        id: '0002',
+        imgUrl: '//tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/fd16ccffb2e2376ff370bda3bebebd71.jpg',
+        desc: '不负嵊夏好时光，泗乎太安逸'
+      },
+      {
+        id: '0003',
+        imgUrl: '//tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/c2ae82fcc9fa11fa253e5919c49bcfbf.jpg',
+        desc: '探访日本，一部家庭旅行画册'
+      },
+      {
+        id: '0004',
+        imgUrl: '//tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/5a11974656794b4715156501be10c1b7.jpg',
+        desc: '厦门，带娃出行与幸福同行'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl'
+@import '~@/assets/styles/mixins.styl'
+  .radiers
+    position: relative
+    height: 9.5rem
+    .title
+      margin-left: .3rem
+      margin-top: .6rem
+      color: #26282a
+      font-weight: 700
+      font-size: .36rem
+    .radiers-title
+      width: 25%
+      line-height: .6rem
+      border-radius: .3rem
+      background: $bgColor
+      color: white
+      padding: 0 .1rem
+      margin-left: .2rem
+      margin-top: .2rem
+      margin-bottom: .2rem
+      text-align: center
+    .radiers-btn
+      position: absolute
+      bottom: 0
+      left: 50%
+      transform: translateX(-50%)
+      width: 5rem
+      height: 1rem
+      margin-bottom: .2rem
+      color: $bgColor
+      font-weight: 400
+      font-size: .32rem
+      background: #fff
+      border: .02rem solid $bgColor
+    .radiersItem
+      position: relative
+      overflow: hidden
+      float: left
+      width: 50%
+      height: 0
+      padding-bottom: 43%
+      .radiersItem-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        box-sizing: border-box
+        padding: 0 .1rem
+        .radiersItem-img-content
+          display: block
+          border-radius: .2rem
+          width: 100%
+          margin: 0 auto
+      .radiersItem-content
+        position: absolute
+        height: .8rem
+        bottom: 0
+        left: 0
+        right: 0
+        padding: 0 .3rem
+        .recomendItem-desc
+          display: block
+          font-size: .14rem
+          color: #000
+          font-weight: 450
+          ellipsis()
+
+</style>
--- src/pages/home/components/hotRecommend.vue
+++ src/pages/home/components/hotRecommend.vue
@@ -1,62 +1,170 @@
 <template>
   <div class="recommend">
+    <div class="title">特惠门票</div>
     <div class="recommend-title">当季热门度假</div>
-    <div class="recommendItem">
+    <div class="recommendItem" v-for="item of recommendList" :key="item.id">
       <div class="recommendItem-img">
-        <img src="//imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg" class="recommendItem-img-content">
+        <img :src="item.imgUrl" class="recommendItem-img-content">
+        <div class="recommendItem-img-desc">
+          <div class="recommendItem-img-desc-wrapper">
+            <div class="recommendItem-img-desc-addition">{{item.descAddition}}</div>
+            <div class="recommendItem-img-desc-day">{{item.descDay}}</div>
+          </div>
+        </div>
       </div>
       <div class="recommendItem-content">
-        <span class="recomendItem-desc">222</span>
-        <p>111</p>
+        <span class="recomendItem-desc">{{item.desc}}</span>
+        <p class="recommendItem-price">{{item.price}}</p>
       </div>
     </div>
+    <button class="recommend-btn">更多当季热门度假 ></button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeHotRecommend'
+  name: 'HomeHotRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: '//imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg',
+        descAddition: '盐城 - 三亚',
+        descDay: '5天跟团游',
+        desc: '含往返飞机票+4晚住宿',
+        price: '￥ 2950'
+      },
+      {
+        id: '0002',
+        imgUrl: '//imgs.qunarzz.com/p/p4/1809/50/c387c050d5188702.jpg_256x160_0d611436.jpg',
+        descAddition: '盐城 - 苏州',
+        descDay: '3天跟团游',
+        desc: '含2晚住宿',
+        price: '￥ 588'
+      },
+      {
+        id: '0003',
+        imgUrl: '//imgs.qunarzz.com/p/p70/1809/e7/4941057a6aae702.jpg_256x160_9fee6ccb.jpg',
+        descAddition: '盐城 - 丽江',
+        descDay: '5天跟团游',
+        desc: '含往返飞机票+4晚住宿',
+        price: '￥ 4960'
+      },
+      {
+        id: '0004',
+        imgUrl: '//imgs.qunarzz.com/p/p27/201302/28/d14185ac8a47f6f993835fbb.jpg_256x160_c2ab9336.jpg',
+        descAddition: '盐城 - 张家界',
+        descDay: '4天自由行',
+        desc: '含往返飞机票+3晚住宿',
+        price: '￥ 2290'
+      }]
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '~@/assets/styles/varibles.styl'
-  .recommend-title
-    width: 25%
-    line-height: .6rem
-    border-radius: .3rem
-    background: $bgColor
-    color: white
-    padding: .1rem
-    margin-left: .2rem
-    margin-top: .2rem
-    margin-bottom: .2rem
-    text-align: center
-  .recommendItem
+@import '~@/assets/styles/mixins.styl'
+  .recommend
     position: relative
-    overflow: hidden
-    float: left
-    width: 50%
-    height: 0
-    padding-bottom: 50%
-    background: red
-    .recommendItem-img
-      position: absolute
-      top: 0
-      left: 0
-      right: 0
-      // bottom: .44rem
-      box-sizing: border-box
+    height: 9.5rem
+    // background: red
+    .title
+      margin-left: .3rem
+      margin-top: .6rem
+      color: #26282a
+      font-weight: 700
+      font-size: .36rem
+    .recommend-title
+      width: 25%
+      line-height: .6rem
+      border-radius: .3rem
+      background: $bgColor
+      color: white
       padding: 0 .1rem
-      .recommendItem-img-content
-        display: block
-        width: 100%
-        margin: 0 auto
-    .recommendItem-content
+      margin-left: .2rem
+      margin-top: .2rem
+      margin-bottom: .2rem
+      text-align: center
+    .recommend-btn
       position: absolute
       bottom: 0
-      left: 0
-      right: 0
-      padding: 0 .1rem
-      background-color: blue;
+      left: 50%
+      transform: translateX(-50%)
+      width: 5rem
+      height: 1rem
+      margin-bottom: .2rem
+      color: $bgColor
+      font-weight: 400
+      font-size: .32rem
+      background: #fff
+      border: .02rem solid $bgColor
+    .recommendItem
+      position: relative
+      overflow: hidden
+      float: left
+      width: 50%
+      height: 0
+      padding-bottom: 43%
+      // background: red
+      .recommendItem-img
+        position: absolute
+        // overflow: hidden
+        // border-radius: .1rem
+        top: 0
+        left: 0
+        right: 0
+        // bottom: .100rem
+        box-sizing: border-box
+        padding: 0 .1rem
+        .recommendItem-img-content
+          display: block
+          border-radius: .2rem
+          width: 100%
+          margin: 0 auto
+        .recommendItem-img-desc
+          position: absolute
+          bottom: 0
+          left: 50%
+          transform: translateX(-50%)
+          background: black
+          opacity: .7
+          .recommendItem-img-desc-wrapper
+            width: 2rem
+            height: 1rem
+            .recommendItem-img-desc-addition
+              text-align: center
+              padding-top: .2rem
+              font-size: .14rem
+              font-weight: 400
+              font-family: Microsoft YaHei
+              color: #fff
+            .recommendItem-img-desc-day
+              text-align: center
+              padding-top: .1rem
+              font-size: .1rem;
+              font-weight: 400;
+              font-family: Microsoft YaHei
+              color: #ffde00;
+      .recommendItem-content
+        position: absolute
+        height: .8rem
+        bottom: 0
+        left: 0
+        right: 0
+        padding: 0 .3rem
+        // background-color: blue
+        .recomendItem-desc
+          display: block
+          font-size: .14rem
+          color: #000
+          font-weight: 450
+          ellipsis()
+        .recommendItem-price
+          margin-top: .1rem
+          font-size: .30rem
+          font-weight: 550
+          color: #ff7400
+
 </style>
